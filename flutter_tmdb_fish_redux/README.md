602b30e9b914dff88588fb79404888600d9e52b1
linus.du <dj13808079752@gmail.com>
2021-01-22 17:35:08 +0800

    Init project

---
+++ flutter_tmdb_fish_redux/README.md
@@ -0,0 +1,2 @@
+# flutter_tmdb_fish_redux
+A Flutter Demo using Fish Redux
---
+++ flutter_tmdb_fish_redux/android/app/src/main/kotlin/linus/flutter/flutter_tmdb_fish_redux/MainActivity.kt
@@ -0,0 +1,6 @@
+package linus.flutter.flutter_tmdb_fish_redux
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
